bcc3d845e72d4f9ff4d2c6aaf9962b580d9e7633
success <2586036379@qq.com>
2026-06-17 21:36:46 +0800

    第一版

---
+++ DFS.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+class Solution
+{
+public:
+    void DFS(vector<vector<int>> &isConnected, vector<int> &visit, int cities, int i)
+    {
+        for (int j = 0; j < cities; j++)
+        {
+            if (isConnected[i][j] == 1 && visit[j] == 0)
+            {
+                visit[j] = 1;
+                DFS(isConnected, visit, cities, j);
+            }
+        }
+    }
+    int findCircleNum(vector<vector<int>> &isConnected)
+    {
+        int cities = isConnected.size();
+        vector<int> visit;
+        visit.resize(cities);
+        int provinces = 0;
+        for (int i = 0; i < cities; i++)
+        {
+            if (visit[i] == 0)
+            {
+                DFS(isConnected, visit, cities, i);
+                provinces++;
+            }
+        }
+        return provinces;
+    }
+};
+int main()
+{
+    vector<vector<int>> test;
+    Solution();
+
+    return 0;
+}
